3a09d523d357b90b6b1636cb1a9d151afb8bdea8
Gavesh Bimsath <91552385+gbimsath@users.noreply.github.com>
2023-09-28 21:18:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("ea8263e0393b6c06d4cf83ca6c5014ad")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    TELEGRAM_API = os.environ["8657438"]
+    OWNER = os.environ.get("1404096450")
+    OWNER_USERNAME = os.environ.get("bimzath")
+    PASSWORD = os.environ.get("bimzath@000")
+    DATABASE_URL = os.environ.get("mongodb+srv://bimzathx:deshx@deshx.vojtx48.mongodb.net/?retryWrites=true&w=majority")
+    LOGCHANNEL = os.environ.get("-1001962430444")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
